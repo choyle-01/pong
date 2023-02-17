0840ca69fcdcddbb9b630e568cc56b004ac5de99
Michael Hoyle <Michael.Hoyle@townsquareinteractive.com>
2023-02-17 13:52:24 -0500

    Finished game timer and score count

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import pygame, sys, random
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y, player_score, opponent_score
+    global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -10,14 +10,18 @@ def ball_animation():
 
     if ball.left <=0: 
         player_score += 1 
-        ball_restart()
+        score_time = pygame.time.get_ticks()
 
     if ball.right >= screen_width:
         opponent_score += 1
-        ball_restart()
+        score_time = pygame.time.get_ticks()
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
+    if ball.colliderect(player) and ball_speed_x > 0:  
         ball_speed_x *= -1
+    
+    if ball.colliderect(opponent) and ball_speed_x < 0:
+        if abs(ball.right - player.left) < 10:
+            ball_speed_x *= -1
 
 def player_animation():
     player.y += player_speed
@@ -37,10 +41,30 @@ def opponent_ai():
         opponent.bottom = screen_height
 
 def ball_restart():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, score_time
+
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width/2, screen_height/2)
-    ball_speed_y *= random.choice((1,-1))
-    ball_speed_x *= random.choice((1,-1))
+
+    if current_time - score_time < 700:
+        number_three = game_font.render('3', False, light_grey)
+        screen.blit(number_three,(screen_width/2 - 10, screen_height/2 + 10))
+
+    if 700 < current_time - score_time < 1400:
+        number_two = game_font.render('2', False, light_grey)
+        screen.blit(number_two,(screen_width/2 - 10, screen_height/2 + 10))
+
+    if 1400 < current_time - score_time < 2100:
+        number_one = game_font.render('1', False, light_grey)
+        screen.blit(number_one,(screen_width/2 - 10, screen_height/2 + 10))
+
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0,0
+    else: 
+        ball_speed_y = 7 * random.choice((1,-1))
+        ball_speed_x = 7 * random.choice((1,-1))
+        score_time = None
+
 
 # General setup
 pygame.init()
@@ -71,10 +95,10 @@ opponent_speed = 7
 # Text Veriables
 player_score = 0 
 opponent_score = 0
-game_font = pygame.font.Font('freesamsbold.ttf', 32)
+game_font = pygame.font.SysFont('freesamsbold.ttf', 32)
 
 # Score Timer
-score_timer = None
+score_time = True
 
 while True:
     # Handling input
@@ -105,6 +129,9 @@ while True:
     pygame.draw.ellipse(screen, ball_color, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
     
+    if score_time:
+        ball_restart()
+
     player_text = game_font.render(f"{player_score}", False, light_grey)
     screen.blit(player_text,(660,470))
 
